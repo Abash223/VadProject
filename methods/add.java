a0d66af83182a52f71cd0f760db4f3c81d046100
Abash223 <abashnaorem@gmail.com>
2025-03-12 14:25:50 +0530

    program

---
+++ methods/add.java
@@ -0,0 +1,10 @@
+public class add{
+    public void add(int num1,int num2){
+        int num3=num1+num2;
+        System.out.println(num3);
+    }
+    public static void main(String args[]){
+        add a=new add();
+        a.add(23,35);
+    }
+}
