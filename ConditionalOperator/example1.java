4284a58032472aaaafa975d0e6701f956c653763
Abash223 <abashnaorem@gmail.com>
2025-03-13 10:31:14 +0530

    Program

---
+++ ConditionalOperator/example1.java
@@ -0,0 +1,11 @@
+public class example1{
+    public static void main(String args[]){
+        System.out.println(12>65);
+         System.out.println(156>=65);
+          System.out.println(12<65);
+           System.out.println(123>65);
+            System.out.println(65==65);
+             System.out.println(12<=65);
+
+    }
+}
