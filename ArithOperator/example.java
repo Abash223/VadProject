56bcc45b53e18202a28196ca6c2bfced00724964
Abash223 <abashnaorem@gmail.com>
2025-03-13 10:22:24 +0530

    Program

---
+++ ArithOperator/example.java
@@ -0,0 +1,11 @@
+public class example{
+    public static void main(String args[])
+    {
+      System.out.println(19+28);
+      System.out.println(23-5);
+      System.out.println(18*3);
+      System.out.println(19/8);
+      System.out.println(19%5);
+
+    }
+}
