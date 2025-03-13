ed52af094cc3386d66f03323be5a7b2f52656031
Abash223 <abashnaorem@gmail.com>
2025-03-13 10:51:28 +0530

    Program

---
+++ ConditionalStatement/example.java
@@ -0,0 +1,11 @@
+public class example{
+    public static void main(String args[]){
+       int age;
+         if(true){
+            System.out.println("I am Abash");
+        }
+        if(false){
+            System.out.println("I don't know");
+        }
+}
+}
