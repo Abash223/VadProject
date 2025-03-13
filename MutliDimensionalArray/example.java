910d5f5b930e4bb7ecf89df9b6b8cc4cfada2e0a
Abash223 <abashnaorem@gmail.com>
2025-03-13 13:39:20 +0530

    Program

---
+++ MutliDimensionalArray/example.java
@@ -0,0 +1,7 @@
+public class example{
+    public static void main(String args[]){
+        int arr[][]={{12,23,45},{12,234,46}};
+        System.out.println(arr[1][2]);
+         System.out.println(arr[1][0]);
+    }
+}
