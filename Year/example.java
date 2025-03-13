8c7f5ce5bffaa1ca5c4c1f87079c9eb1bd25f466
Abash223 <abashnaorem@gmail.com>
2025-03-13 14:36:52 +0530

    Program

---
+++ Year/example.java
@@ -0,0 +1,13 @@
+public class example{
+    public static void main(String args[]){
+         int total=0;
+         for(int i=2020;i<2031;i++){
+             if (i%4==0)
+             {
+                total=total+1;
+                
+             }
+        }
+         System.out.println(total);
+    }
+}
