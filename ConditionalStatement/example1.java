c44a23fb47b6745b022348ceaa5151efd733262f
Abash223 <abashnaorem@gmail.com>
2025-03-13 10:57:48 +0530

    Program

---
+++ ConditionalStatement/example1.java
@@ -0,0 +1,10 @@
+public class example1{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("You are not good");
+        }
+        else{
+            System.out.println("you are good");
+        }
+    }
+}
