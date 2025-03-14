b81768e6c8268d9876e98d0cddd0f19b28ce57ec
Abash223 <abashnaorem@gmail.com>
2025-03-14 10:03:05 +0530

    Program

---
+++ while/example.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class example{
+    public static void main(String args[]){
+        int i=92;
+        while(i<99){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
