dc12d7df6a8b6e73757441dde5d724b2f3ec1452
Abash223 <abashnaorem@gmail.com>
2025-03-14 10:15:59 +0530

    Program

---
+++ 7Table/example.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class example{
+    public static void main(String args[]){ 
+        int i=1;
+        while(i<=10){
+            System.out.println("7 * " +i+ "=" +7*i);
+            i++;
+    
+        }
+
+    }
+}
