b5a590d861eb272676bde573ddaeccc1052ddee2
Abash223 <abashnaorem@gmail.com>
2025-03-14 10:24:09 +0530

    Program

---
+++ nNumbers/example.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class example{
+    public static void main(String args[]){
+        int i=1;  
+    
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enetr the number");
+        int n=input.nextInt();
+        while(i<=n){ 
+            System.out.println(i);
+        i++;
+        }
+    }
+}
