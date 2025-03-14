89370b1f97969a4165ce0585990beb91423e76ed
Abash223 <abashnaorem@gmail.com>
2025-03-14 12:01:31 +0530

    Program

---
+++ UpperlowerCase/example.java
@@ -0,0 +1,7 @@
+public class example{
+    public static void main(String args[]){
+        String n="Hello World";
+        System.out.println(n.toUpperCase());
+        System.out.println(n.toLowerCase());
+    }
+}
