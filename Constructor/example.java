57ac9d22ac1182084b47d486942861c57b71c0af
Abash223 <abashnaorem@gmail.com>
2025-03-15 10:54:32 +0530

    Program

---
+++ Constructor/example.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class example{
+    public example(){
+        System.out.println("adsa");
+    }
+    
+    public static void main(String args[]){
+        example obj=new example();
+
+    }
+}
